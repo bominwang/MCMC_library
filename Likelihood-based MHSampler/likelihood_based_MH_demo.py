fb5306fc6eb5f41c732f09f4b0ac0cd729e8cd29
bominwang <87200252+bominwang@users.noreply.github.com>
2023-08-20 06:30:23 +0800

    Add files via upload

--- Likelihood-based MHSampler/likelihood_based_MH_demo.py
+++ Likelihood-based MHSampler/likelihood_based_MH_demo.py
@@ -41,6 +41,16 @@ class MixtureNormal2d(object):
 
         return mixture_prob
 
+def complex_mixture_pdf(x):
+    component_means = np.array([[2, 2], [6, 6], [10, 2]])
+    component_covs = np.array([[[1, 0.5], [0.5, 1]],[[1, -0.3], [-0.3, 1]],[[1, 0], [0, 1]]])
+    component_weights = np.array([0.4, 0.3, 0.3])
+    logpdf_vals = [np.log(weight) + scipy.stats.multivariate_normal(mean, cov).logpdf(x) for weight, mean, cov in zip(component_weights,
+                                                                                                                      component_means,
+                                                                                                                      component_covs)]
+    total_logpdf = np.logaddexp.reduce(logpdf_vals)
+    return total_logpdf
+
 
 if __name__ == '__main__':
 
@@ -68,7 +78,7 @@ if __name__ == '__main__':
                 label=f'Generated Samples (Histogram) i={i + 1}')
         ax.set_xlabel('x')
         ax.set_ylabel('Probability Density')
-        ax.set_title(f'Comparison of True PDF and Generated Samples (Histogram) i={i + 1}')
+        # ax.set_title(f'Comparison of True PDF and Generated Samples (Histogram) i={i + 1}')
         ax.legend()
 
     plt.tight_layout()
@@ -85,8 +95,8 @@ if __name__ == '__main__':
                                  proposal_distribution='normal',
                                  transfer_method='component-wise',
                                  gpu_flag=False).run(initial_position=initial_state,
-                                                     steps_warm_up=10000,
-                                                     num_samples=10000,
+                                                     steps_warm_up=1000,
+                                                     num_samples=1000,
                                                      width=width,
                                                      sigma=sigma)
     mixture = MixtureNormal2d(log_flag=False)
@@ -99,13 +109,54 @@ if __name__ == '__main__':
     fig, axes = plt.subplots(2, 2, figsize=(12, 10))
 
     for i, ax in enumerate(axes.flatten()):
-        ax.scatter(samples[:, i, 0], samples[:, i, 1], s=10, color='blue', alpha=0.2,
-                   label=f'Generated Samples i={i + 1}')
-        ax.contour(X, Y, pdf_values, levels=20, colors='red')
+        contour = ax.contourf(X, Y, pdf_values, levels=20, cmap='viridis', alpha=0.4)
+        ax.scatter(samples[:, i, 0], samples[:, i, 1], s=10, color='red', alpha=1,label=f'Generated Samples i={i + 1}')
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
-        ax.set_title(f'2D Mixture Gaussian PDF (Contour) and Generated Samples (Scatter) i={i + 1}')
+        # ax.set_title(f'2D Mixture Gaussian PDF (Contour) and Generated Samples (Scatter) i={i + 1}')
         ax.legend()
-
     plt.tight_layout()
+    plt.show()
+
+    """
+    2d mixture_pdf
+    """
+
+    initial_state = np.array([[0, 0], [3, 0], [0, 4], [2, 3]]).reshape(4, -1)
+    width = np.array([1.0, 2.0]).reshape(1, 2)
+    sigma = np.array([1.0, 2.0]).reshape(1, 2)
+    samples = MetropolisHastings(target_distribution=complex_mixture_pdf,
+                                 proposal_distribution='uniform',
+                                 transfer_method='component-wise',
+                                 gpu_flag=False).run(initial_position=initial_state,
+                                                     steps_warm_up=1000,
+                                                     num_samples=1000,
+                                                     width=width,
+                                                     sigma=sigma)
+    x, y = np.meshgrid(np.linspace(-2, 14, 300), np.linspace(-2, 10, 300))
+    pos = np.dstack((x, y))
+    pdf_values = complex_mixture_pdf(pos)
+
+    fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+    for i, ax in enumerate(axes.flatten()):
+        contour = ax.contourf(x, y, pdf_values, levels=20, cmap='viridis', alpha=0.4)
+        ax.scatter(samples[:, i, 0], samples[:, i, 1], s=10, color='red', alpha=1, label=f'Generated Samples i={i + 1}')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.legend()
+        # ax.set_title(f'Complex 2D Mixture Distribution Contour Plot (Dimension {i+1})')
+    plt.tight_layout()
+    plt.show()
+
+    """
+    Moving Trajectory
+    """
+    positions = samples[:, 0, :].squeeze()
+    plt.figure(figsize=(8, 6))
+    plt.contourf(x, y, pdf_values, levels=20, cmap='viridis', alpha=0.4)
+    plt.plot(positions[:, 0], positions[:, 1], marker='o', linestyle='-')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Moving Trajectory')
+    plt.grid(True)
     plt.show()
